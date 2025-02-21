75837bface71cc4101517034026da3e0bfec75f2
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-02-21 21:01:53 +0800

    feat(yakumo-rolldown): 支持基本功能

---
+++ packages/yakumo-rolldown/readme.md
@@ -0,0 +1,7 @@
+# yakumo-esbuild
+
+[![downloads](https://img.shields.io/npm/dm/yakumo-rolldown?style=flat-square)](https://www.npmjs.com/package/yakumo-rolldown)
+[![npm](https://img.shields.io/npm/v/yakumo-rolldown?style=flat-square)](https://www.npmjs.com/package/yakumo-rolldown)
+[![GitHub](https://img.shields.io/github/license/Hoshino-Yumetsuki/kumoya?style=flat-square)](https://github.com/Hoshino-Yumetsuki/kumoya/blob/master/LICENSE)
+
+[Esbuild](https://esbuild.github.io) support for [Yakumo](https://github.com/shigma/yakumo).
---
+++ packages/yakumo-rolldown/src/index.ts
@@ -0,0 +1,19 @@
+import { Context } from 'yakumo'
+import { load } from 'tsconfig-utils'
+import kumoya from 'kumoya'
+
+export const inject = ['yakumo']
+
+export function apply(ctx: Context) {
+    ctx.register('rolldown', async () => {
+        const paths = ctx.yakumo.locate(ctx.yakumo.argv._)
+        await Promise.all(
+            paths.map(async (path) => {
+                const cwd = ctx.yakumo.cwd + path
+                const tsconfig = await load(cwd).catch(() => null)
+                if (!tsconfig) return
+                await kumoya(cwd, ctx.yakumo.workspaces[path], tsconfig)
+            })
+        )
+    })
+}
